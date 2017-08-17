5cce41507ce3c4138833795e720423c9ee498169
DaiGeun Sohn <nhos126@gmail.com>
2017-08-17 16:02:35 +0900

    reindent lines

--- app.js
+++ app.js
@@ -7,30 +7,30 @@ app.get('/', (req, res) => {
 
 	var options = {
 		token: {
-		key: 'AuthKey_WV97FAUVT3.p8',
-		keyId: 'WV97FAUVT3',
-		teamId: '23EFP3FERN'
+			key: 'AuthKey_WV97FAUVT3.p8',
+			keyId: 'WV97FAUVT3',
+			teamId: '23EFP3FERN'
 		},
 		production: false
 	};
 
-var apnProvider = new apn.Provider(options);
+	var apnProvider = new apn.Provider(options);
 
-let deviceToken = 'DC93EA6E671105011E641D2C81AA621EBAA4BC0B96F0722D649712303F33E940'
+	let deviceToken = 'DC93EA6E671105011E641D2C81AA621EBAA4BC0B96F0722D649712303F33E940'
 
-var note = new apn.Notification();
+	var note = new apn.Notification();
 
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now. 
-note.badge = 3;
+	note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now. 
+	note.badge = 1;
 	note.sound = "ping.aiff";
-	note.alert = "KimDadong";
+	note.alert = "New diary from mom";
 	note.payload = {'messageFrom': 'John Appleseed'};
-note.topic = "com.Sohn.CocoaBaby";
+	note.topic = "com.Sohn.CocoaBaby";
 
-apnProvider.send(note, deviceToken).then( (result) => {
+	apnProvider.send(note, deviceToken).then( (result) => {
 		// see documentation for an explanation of result 
 		console.log(result)
-		});
+	});
 })
 
 app.listen(3000, () => console.log('Server running on port 3000!'))
