d7c3fc2b137d7de30d2eeaa3bb96875251c31a59
DaiGeun Sohn <nhos126@gmail.com>
2017-08-18 14:23:21 +0900

    add res.send()

--- app.js
+++ app.js
@@ -88,9 +88,11 @@ app.get('/notification/comment/:token', (req, res) => {
 	// note.payload = {'messageFrom': 'John Appleseed'};
 	note.topic = "com.Sohn.CocoaBaby";
 
+	res.send('Success');
+	
 	apnProvider.send(note, deviceToken).then( (result) => {
 		// see documentation for an explanation of result 
-		res.send('Success');
+
 		console.log(result)
 	});
 })
